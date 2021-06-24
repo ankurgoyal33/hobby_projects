c136e741e38dc66005e107a27519acd445994f85
ankurgoyal33 <coolankur33@gmail.com>
2021-06-24 09:33:46 +0530

    diff ways to add images COMMIT

--- src/components/ProjectViewerModal.js
+++ src/components/ProjectViewerModal.js
@@ -25,7 +25,12 @@ const ProjectViewerModal = (props) => {
                     <h2>Details of Selected Project</h2>
                     <p>TITLE:{props.showProjectViewerModal.title }</p>
                     <p>DESC: {props.showProjectViewerModal.description}</p>
+                    <br />
+                    {/* <img src={logo} alt="logo" /> */}
+                    {/* <img src={require('/logo.png')} /> */}
                     {/* <img src={"../../../../BingWallpaper.jpg"} /> */}
+                    <img src="/logo.png" />
+
                     <button onClick={handleCloseProjectViewerModal}>Ok</button>
                 </div>
             }
